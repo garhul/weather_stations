b56e0235e0550de7b5b8761dc2966447c1c81ab7
garhul <adrian.gesto@gmail.com>
2020-07-29 01:18:08 +0200

    coso

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "30641"
+  #define BUILD_NUMBER "30647"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.30641 - 2020-07-29 01:17:39.928636"
+  #define VERSION "v0.1.30647 - 2020-07-29 01:18:03.219262"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.30641"
+  #define VERSION_SHORT "v0.1.30647"
 #endif
