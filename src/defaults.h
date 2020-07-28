a6def7bf9e7b33a6a5f458c78a0ae340b5cdc18c
garhul <adrian.gesto@gmail.com>
2020-07-29 01:17:43 +0200

    Initial commit

--- src/defaults.h
+++ src/defaults.h
@@ -1,7 +1,15 @@
 #ifndef __DEFAULTS_H__
 #define __DEFAULTS_H__
 
-#define AWAKE_TEST true
-#define CONN_RETRIES 3
 
+
+
+//TODO:: move this to EEPROM config
+#define VBAT_MULTIPLIER 0.0198f
+#define SLEEP_TIME 10e6 //microseconds
+#define LOCAL_SSID 
+#define LOCAL_PWD 
+#define BROKER "192.168.0.10"
+#define TOPIC "home/living"
+#define CONN_RETRIES 3
 #endif
--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "30540"
+  #define BUILD_NUMBER "30641"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.30540 - 2020-07-29 01:11:09.130695"
+  #define VERSION "v0.1.30641 - 2020-07-29 01:17:39.928636"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.30540"
+  #define VERSION_SHORT "v0.1.30641"
 #endif
