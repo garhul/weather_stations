11e24fb781efb2de9315beb79cda824bc337550f
garhul <adrian.gesto@gmail.com>
2020-07-30 03:28:45 +0200

    remove unnecessary data

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "54208"
+  #define BUILD_NUMBER "54219"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.54208 - 2020-07-30 03:27:53.006647"
+  #define VERSION "v0.1.54219 - 2020-07-30 03:28:36.317804"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.54208"
+  #define VERSION_SHORT "v0.1.54219"
 #endif
